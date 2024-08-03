9c52e8c02d577bf8271c26f414576c5f06ec01b3
codewith-sameer <workwithsameer.n@gmail.com>
2024-08-03 17:41:35 +0530

    Updated tests and main.yml

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,13 @@
 import pytest
 from flask.testing import FlaskClient
 import json
+import sys
+import os
 from app import app
+
+# Add the src directory to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                             "../src")))
 
 
 @pytest.fixture
